bbae14aedd045649b7e67bad03edf12053d839c7
Adam Pacholski <apachols@gmail.com>
2022-04-18 17:07:51 -0700

    Add fullscreen, mouse events for os x, and tablet/canvas code commented out

--- touchscreen/touchscreen_view.py
+++ touchscreen/touchscreen_view.py
@@ -1,6 +1,11 @@
+# https://github.com/pyglet/pyglet/blob/master/examples/input/tablet.py
+
 # importing pyglet module
 import pyglet
 
+FULLSCREEN = False
+
+# Scale factor is irrelevant if FULLSCREEN=True
 SCALE_FACTOR = 0.5
 
 # width of window
@@ -10,7 +15,7 @@ width = 2405 * SCALE_FACTOR
 height = 1367 * SCALE_FACTOR
 
 # creating a window
-window = pyglet.window.Window(width, height, 'lightdream')
+window = pyglet.window.Window(width, height, 'lightdream', fullscreen=FULLSCREEN)
 
 # render the layout
 image = pyglet.image.load('../images/!touchscreen layout 3.png')
@@ -26,6 +31,42 @@ def on_draw():
 
     # draw the image on screen
     sprite.draw()
+
+# TODO
+# The tablets interface unavailable on OS X, per the pyglet docs
+#   http://pyglet-current.readthedocs.io/en/latest/api/pyglet/input/pyglet.input.get_tablets.html
+#
+# For now, let's model some single-finger press/release/drag via the mouse events UI
+#
+# # # # # # # # # # # # # # # # # # # # # # # #
+#
+# tablet = pyglet.input.get_tablets()[0]  # ... there can be only one
+#
+# canvas = tablet.open(window)
+#
+# @canvas.event
+# def on_enter(cursor):
+#     print('%s: on_enter(%r)' % (name, cursor))
+# @canvas.event
+# def on_leave(cursor):
+#     print('%s: on_leave(%r)' % (name, cursor))
+# @canvas.event
+# def on_motion(cursor, x, y, pressure, tilt_x, tilt_y):
+#     print('%s: on_motion(%r, %r, %r, %r, %r, %r)' % (name, cursor, x, y, pressure, tilt_x, tilt_y))
+#
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    print('mouse_press', x, y)
+
+@window.event
+def on_mouse_release(x, y, button, modifiers):
+    print('mouse_release', x, y)
+
+@window.event
+def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
+    print('mouse_drag', x, y, dx, dy, buttons, modifiers)
 
 # start running the application
 pyglet.app.run()
