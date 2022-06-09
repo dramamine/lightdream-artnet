9cab82e772d34b728c75a0fcf41fb42178835484
Adam Pacholski <apachols@gmail.com>
2022-06-08 19:38:14 -0700

    promote kivy to touchscreen_view.py

--- touchscreen_view.py
+++ touchscreen_view.py
@@ -1,4 +1,9 @@
-import pyglet
+from kivy.app import App
+from kivy.config import Config
+from kivy.core.image import Image
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 from touchscreen_circles import HUESHIFT
 from touchscreen_circles import KALEIDOSCOPE
@@ -11,188 +16,108 @@ from touchscreen_circles import RADIANTLINES
 from touchscreen_input import InputCoordinateMapper
 
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# TODO
-# <0> Add touch events
-# <1> Possibly fix filter / circle naming convention
-# <2> Add RIGHT side circles
-# <3> Implement other controls
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-# Set this to true to render the full layout png
-# This is useful for testing the input_mapper
-LAYOUT_TEST = True
-
-# Set this to true when running in production
-FULLSCREEN = False
-
-# The full image dimensions of the layout image
-LAYOUT_IMAGE_WIDTH = 2405
-LAYOUT_IMAGE_HEIGHT = 1357
-
-# Scale factor is irrelevant if FULLSCREEN=True
-LAYOUT_TEST_SCALE_FACTOR = 0.5
-
-# width of window
-width = LAYOUT_IMAGE_WIDTH * LAYOUT_TEST_SCALE_FACTOR
-
-# height of window
-height = LAYOUT_IMAGE_HEIGHT * LAYOUT_TEST_SCALE_FACTOR
-
-# creating a window
-window = pyglet.window.Window(width=int(width), height=int(height), caption='lightdream', fullscreen=FULLSCREEN) 
-
-# keep the real WxH for scaling
-(WINDOW_WIDTH, WINDOW_HEIGHT) = window.get_size()
+LAYOUT_IMAGE_WIDTH = 1920
+LAYOUT_IMAGE_HEIGHT = 1080
 
 
-# tells the FingerManager which circles have active coordinates
+Config.set('graphics', 'width', LAYOUT_IMAGE_WIDTH)
+Config.set('graphics', 'height', LAYOUT_IMAGE_HEIGHT)
+
+
 input_mapper = InputCoordinateMapper(LAYOUT_IMAGE_WIDTH)
 
 
-# when detecting circles, use coordinates scaled to the layout image
-def layout_image_coordinates(x, y):
-    scaled_x = round((x / WINDOW_WIDTH) * LAYOUT_IMAGE_WIDTH)
-    scaled_y = round((y / WINDOW_HEIGHT) * LAYOUT_IMAGE_HEIGHT)
-    return (scaled_x, scaled_y)
+class Touchable(Screen):
+    def on_touch_down(self, touch):
+        point = (touch.x, touch.y)
+        # print("============================>point", point)
+        input_mapper.process_touch_enter(touch.id, point)
+
+        # Annoying: touch bindings on the Touchable screens override all button
+        # press bindings.  DebugMenu screen has working on_press() bindings,
+        # and shouldn't need to check for button collisions in the touch handler.
+        if self.ids.NEXT_SCREEN_BUTTON.collide_point(*touch.pos):
+            self.next_screen_callback(touch)
+
+        return super().on_touch_down(touch)
+
+    def on_touch_move(self, touch):
+        point = (touch.x, touch.y)
+        input_mapper.process_touch_motion(touch.id, point)
+        return super().on_touch_move(touch)
+
+    def on_touch_up(self, touch):
+        point = (touch.x, touch.y)
+        input_mapper.process_touch_leave(touch.id)
+        return super().on_touch_up(touch)
+
+    def next_screen_callback(self):
+        pass
 
 
-# when placing sprites, use coordinates scaled to the window
-def window_coordinates(x, y):
-    scaled_x = round((x / LAYOUT_IMAGE_WIDTH) * WINDOW_WIDTH)
-    scaled_y = round((y / LAYOUT_IMAGE_HEIGHT) * WINDOW_HEIGHT)
-    return (scaled_x, scaled_y)
+class LightdreamTouchScreen(Touchable):
+    title = 'Lightdream'
+    CIRCLES = {
+        'HUESHIFT': HUESHIFT,
+        'KALEIDOSCOPE': KALEIDOSCOPE,
+        'TUNNEL': TUNNEL,
+        'LIGHTNING': LIGHTNING,
+        'NUCLEAR': NUCLEAR,
+        'SPIRAL': SPIRAL,
+        'RADIANTLINES': RADIANTLINES,
+    }
+    def __init__(self):
+        super().__init__()
+        for circle in self.CIRCLES.keys():
+            self.ids[circle].source = self.CIRCLES[circle].path
+            self.ids[circle].center_x = self.CIRCLES[circle].center[0]
+            self.ids[circle].center_y = self.CIRCLES[circle].center[1]
+
+    def update_active(self):
+        for circle in self.CIRCLES.keys():
+            circle_config = self.CIRCLES[circle]
+            if input_mapper.is_active(circle_config.key):
+                self.ids[circle].source = circle_config.active_path
+            else:
+                self.ids[circle].source = circle_config.path
+
+    def on_touch_down(self, touch):
+        self.update_active()
+        return super().on_touch_down(touch)
+
+    def on_touch_move(self, touch):
+        self.update_active()
+        return super().on_touch_move(touch)
+
+    def on_touch_up(self, touch):
+        self.update_active()
+        return super().on_touch_up(touch)
+
+    def next_screen_callback(self, touch):
+        self.manager.current = 'debug_menu'
+        self.manager.title = 'Debug Menu'
 
 
-def get_layout_test_sprite():
-    image = pyglet.image.load('./images/!touchscreen layout 3.png')
-    sprite = pyglet.sprite.Sprite(image, x=0, y=0)
-    sprite.scale = LAYOUT_TEST_SCALE_FACTOR
-    return sprite
+class DebugMenuScreen(Screen):
+    def next_screen_callback(self, touch):
+        self.manager.current = 'layout_test'
+        self.manager.title = 'Layout Test'
 
 
-def get_circle_sprite(circle, active=False):
-    path = circle.path
-    if active:
-        path = path.replace('.png', '-active.png')
-    image = pyglet.image.load(path)
-
-    # use window coords, instead of layout image coords, for image placement
-    lower_left = window_coordinates(circle.X, circle.Y)
-
-    # sprites are placed from the lower left corner
-    sprite = pyglet.sprite.Sprite(image, x=lower_left[0], y=lower_left[1])
-
-    # scales the image down to the right size for the layout
-    sprite.scale = circle.SCALE
-
-    return sprite
+class LayoutTestScreen(Touchable):
+    def next_screen_callback(self, touch):
+        self.manager.current = 'lightdream'
+        self.manager.title = 'Lightdream'
 
 
-def draw_circle(key, sprite_pair):
-    if input_mapper.is_active(key):
-        sprite_pair[1].draw()
-    else:
-        sprite_pair[0].draw()
+class MainApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(LightdreamTouchScreen(), name='lightdream')
+        sm.add_widget(DebugMenuScreen(), name='debug_menu')
+        sm.add_widget(LayoutTestScreen(), name='layout_test')
+        return sm
 
 
-sprite_pair_hueshift = [
-    get_circle_sprite(HUESHIFT),
-    get_circle_sprite(HUESHIFT, active=True)
-]
-sprite_pair_kaleidoscope = [
-    get_circle_sprite(KALEIDOSCOPE),
-    get_circle_sprite(KALEIDOSCOPE, active=True)
-]
-sprite_pair_tunnel = [
-    get_circle_sprite(TUNNEL),
-    get_circle_sprite(TUNNEL, active=True)
-]
-sprite_pair_lightning = [
-    get_circle_sprite(LIGHTNING),
-    get_circle_sprite(LIGHTNING, active=True)
-]
-sprite_pair_nuclear = [
-    get_circle_sprite(NUCLEAR),
-    get_circle_sprite(NUCLEAR, active=True)
-]
-sprite_pair_spiral = [
-    get_circle_sprite(SPIRAL),
-    get_circle_sprite(SPIRAL, active=True)
-]
-sprite_pair_radiantlines = [
-    get_circle_sprite(RADIANTLINES),
-    get_circle_sprite(RADIANTLINES, active=True)
-]
-
-
-# layout test sprite - for coordinate testing purposes only
-sprite_layout = get_layout_test_sprite()
-
-
-# on draw event
-@window.event
-def on_draw():
-
-    # clear the window
-    window.clear()
-
-    if LAYOUT_TEST:
-        sprite_layout.draw()
-        return
-
-    draw_circle(HUESHIFT.key, sprite_pair_hueshift)
-    draw_circle(KALEIDOSCOPE.key, sprite_pair_kaleidoscope)
-    draw_circle(TUNNEL.key, sprite_pair_tunnel)
-    draw_circle(LIGHTNING.key, sprite_pair_lightning)
-    draw_circle(NUCLEAR.key, sprite_pair_nuclear)
-    draw_circle(SPIRAL.key, sprite_pair_spiral)
-    draw_circle(RADIANTLINES.key, sprite_pair_radiantlines)
-
-
-@window.event
-def on_mouse_press(x, y, button, modifiers):
-    point = layout_image_coordinates(x,y)
-    print("point", point)
-    input_mapper.process_mouse_down(point)
-
-
-@window.event
-def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
-    point = layout_image_coordinates(x,y)
-    input_mapper.process_mouse_down(point)
-
-
-@window.event
-def on_mouse_release(x, y, button, modifiers):
-    point = layout_image_coordinates(x,y)
-    input_mapper.process_mouse_up()
-
-
-# TODO - Tablet event listeners
-#
-# https://github.com/pyglet/pyglet/blob/master/examples/input/tablet.py
-#
-# The tablets interface unavailable on OS X, per the pyglet docs
-#   http://pyglet-current.readthedocs.io/en/latest/api/pyglet/input/pyglet.input.get_tablets.html
-#
-# # # # # # # # # # # # # # # # # # # # # # # #
-#
-# tablet = pyglet.input.get_tablets()[0]  # ... there can be only one
-#
-# canvas = tablet.open(window)
-#
-# @canvas.event
-# def on_enter(cursor):
-#     print('%s: on_enter(%r)' % (name, cursor))
-# @canvas.event
-# def on_leave(cursor):
-#     print('%s: on_leave(%r)' % (name, cursor))
-# @canvas.event
-# def on_motion(cursor, x, y, pressure, tilt_x, tilt_y):
-#     print('%s: on_motion(%r, %r, %r, %r, %r, %r)' % (name, cursor, x, y, pressure, tilt_x, tilt_y))
-#
-# # # # # # # # # # # # # # # # # # # # # # # #
-
-
-pyglet.app.run()
+if __name__ == '__main__':
+    MainApp().run()
