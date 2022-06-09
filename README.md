4caf1d871e5a8e44c4fc861bb3f907c0667eee5a
Adam Pacholski <apachols@gmail.com>
2022-06-08 22:08:00 -0700

    Even just a little more cleanup

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Project to run lightdream-scripts code on an rpi instead.
 
-
-
 ```
 
 
@@ -35,6 +33,7 @@ Features:
 ### Touchscreen Interface
 
 Milestone 0: debug menu
+
 - ability to toggle autoplay(DJ mode) vs. sequenced songs mode
 - ability to add song to queue
   - ex. `audio_player.queue(id: string)`
@@ -42,11 +41,13 @@ Milestone 0: debug menu
   - marten to figure this out
 
 Milestone 1: simple on/off effects
+
 - "mask" style: blobs, nuclear, spiral, radiant lines
 - "replace" style: lightning bolts
 - parameters: boolean
 
 Milestone 2: position-based effects
+
 - "mask" style
   - rings:
     - parameters: distance from center (0.0 - 1.0)
@@ -55,35 +56,39 @@ Milestone 2: position-based effects
     - parameters: theta (0.0 at top, to 1.0, going clockwise)
     - animation: show a wedge where the fingers are
 - "filter" style
+
   - huewheel:
     - parameters: theta (0.0 at top, to 1.0, going clockwise)
     - animation: ??
-  
+
 - complex "mask" style
   - rainbow spotlight
     - parameters: (x,y) values where x and y are 0.0 - 1.0
     - animation: rainbow wheel where the fingers are?
 
 Milestone 3: new effects?
+
 - color inverter
 - turn some auto clips into simple on/off effects
 
 Milestone 4: finalize arrangement
 
-Fit N Finish
------
-- make sure initial state of touchscreen works and is not annoying
-- make sure layout works in Fullscreen mode
+## Fit N Finish
 
+- Fullscreen mode
+- Configure the rest of the circles
+- Spotlight effect on the spotlight circle
+- Remove layout test from production
 
 ### Other Stuff
 
 - DJ mode: use FFT to listen for bass
-  + "get energy" cmd
-  - create filters based on that energy
+  - "get energy" cmd
+  * create filters based on that energy
 - calibration settings (order of artnet ip's)
-  + it exists
-  - need a way to toggle metronome
+  - it exists
+  * need a way to toggle metronome
 - respond to keyboard / touchscreen events
 - using alsaaudio for input but pyglet for output. is that OK or bad?
-+ add envs so that we're not importing bad things. prod vs dev? or platform-specific?
+
+* add envs so that we're not importing bad things. prod vs dev? or platform-specific?
--- touch_circles.py
+++ touch_circles.py
@@ -4,30 +4,13 @@ from effects.filters import FilterNames
 class TouchscreenCircle:
     """
     TouchscreenCircle Assumptions
-
-     - uses SCALED coordinates to position each circle in the layout
-     - each image path is for the inactive image; for xyz.png, assume xyz-active.png exists
-
-    full_image_radius:
-
-     - this is the radius, in SCALED pixels, of each circle when it is displayed
-       at its file-size width.
-     - to adjust the fit between touch-target and image, adjust full_image_radius.
-
+     - each image path is for the inactive image; for img.png, assume img-active.png exists
     """
-    def __init__(self, name='', path='', key='', center=None, radius=0, full_image_radius=0):
-        self.name = name
+    def __init__(self, path='', key='', center=None, radius=0):
         self.path = path
         self.key = key
         self.center = center
         self.radius = radius
-        self.full_image_radius = full_image_radius
-
-    # we can use this to place the image sprites
-    def lower_left_corner(self):
-        x = self.center[0] - self.radius
-        y = self.center[1] - self.radius
-        return (x, y)
 
     @property
     def X(self):
@@ -38,70 +21,50 @@ class TouchscreenCircle:
         return self.lower_left_corner()[1]
 
     @property
-    def SCALE(self):
-        return self.radius / self.full_image_radius
-
-    @property
     def active_path(self):
         return self.path.replace('.png', '-active.png')
 
 
-# LEFT bigger circles
 HUESHIFT = TouchscreenCircle(
     path='./images/colorwheel-dithered.png',
     key=FilterNames.HUESHIFT,
-    name='HUESHIFT',
     center=(250, 545),
     radius=210,
-    full_image_radius=800
 )
 KALEIDOSCOPE = TouchscreenCircle(
     path='./images/kaleidoscope.png',
     key=FilterNames.KALEIDOSCOPE,
-    name='KALEIDOSCOPE',
     center=(681, 812),
     radius=210,
-    full_image_radius=800
 )
 TUNNEL = TouchscreenCircle(
     path='./images/tunnel.png',
     key=FilterNames.TUNNEL,
-    name='TUNNEL',
     center=(677, 228),
     radius=210,
-    full_image_radius=800
 )
-# LEFT smaller circles
 LIGHTNING = TouchscreenCircle(
     path='./images/lightning.png',
     key=FilterNames.LIGHTNING,
-    name='LIGHTNING',
     center=(333, 114),
     radius=80,
-    full_image_radius=500
 )
 NUCLEAR = TouchscreenCircle(
     path='./images/nuclear.png',
     key=FilterNames.NUCLEAR,
-    name='NUCLEAR',
     center=(130, 206),
     radius=80,
-    full_image_radius=500
 )
 SPIRAL = TouchscreenCircle(
     path='./images/spiral.png',
     key=FilterNames.SPIRAL,
-    name='SPIRAL',
     center=(1549, 965),
     radius=80,
-    full_image_radius=500
 )
 RADIANTLINES = TouchscreenCircle(
     path='./images/radiant-lines.png',
     key=FilterNames.RADIANTLINES,
-    name='RADIANTLINES',
     center=(1751, 873),
     radius=80,
-    full_image_radius=500
 )
 
