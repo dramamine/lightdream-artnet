783abf6033a35d7ebb33d89db6331d90560c56b9
marten silbiger <marten@metal-heart.org>
2022-07-05 16:22:40 -0700

    try pyartnet

--- modules/artnet.py
+++ modules/artnet.py
@@ -1,4 +1,4 @@
-from stupidArtnet import StupidArtnet
+from pyartnet import ArtNetNode, DmxUniverse
 from util.config import config
 
 def flatten(list_of_lists):
@@ -11,6 +11,9 @@ ips = [
   '169.254.18.35', 
   '169.254.18.36',
 ]
+nodes = [ArtNetNode(host=ip, max_fps=40, refresh_every=0) for ip in ips]
+for node in nodes:
+  node.start()
 
 # TARGET_IP   = DEFAULT 127.0.0.1
 # UNIVERSE    = DEFAULT 0
@@ -18,8 +21,10 @@ ips = [
 # FRAME_RATE  = DEFAULT 30
 # ISBROADCAST = DEFAULT FALSE
 
+universe_lists = list(map( lambda node: [node.add_universe(uni) for uni in range(6)], nodes ))
 
-universe_lists = list(map( lambda ip: [StupidArtnet(ip, uni, 510, 40) for uni in range(6)], ips ))
+for universe_list in universe_lists:
+  [uni.add_channel(start=1, width=510) for uni in universe_list]
 
 # how are the brains arranged? how is the touchscreen oriented to the dome?
 # configs let us rearrange what data is sent to what ip
@@ -38,17 +43,19 @@ brain_positions = [
 
 # universes: list of 6 Artnet instances
 # channels: list of 6 sets of channel data
-def _send_to_brain(universes, channels):
+def _send_to_brain(universes: list[DmxUniverse], channels):
   assert len(universes) == 6
   assert len(channels) == 6
 
   for i in range(6):
-    universes[i].send(bytearray(list(channels[i])))
+    universes[i].data = bytearray(list(channels[i]))
 
 
 def show(frame):
   assert len(frame) == 30
+  positions = brain_positions[config.read("brain_position")]
   for i in range(5):
-    positions = brain_positions[config.read("brain_position")]
     universe_lists_idx = positions[i]
     _send_to_brain(universe_lists[universe_lists_idx], frame[6*i:6*i+6])
+  [node.update() for node in nodes]
+  
