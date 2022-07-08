4b85948500f783b7146f47141d6963fe37f2d2fe
marten silbiger <marten@metal-heart.org>
2022-07-07 19:54:12 -0700

    better subprocess

--- modules/audio_input/subprocess_pyaudio.py
+++ modules/audio_input/subprocess_pyaudio.py
@@ -34,16 +34,9 @@ stream = p.open(format=pyaudio_format,
                 input=True,
                 frames_per_buffer=buffer_size)
 
-if len(sys.argv) > 1:
-    # record 5 seconds
-    output_filename = sys.argv[1]
-    record_duration = 5 # exit 1
-    outputsink = aubio.sink(sys.argv[1], samplerate)
-    total_frames = 0
-else:
-    # run forever
-    outputsink = None
-    record_duration = None
+# run forever
+outputsink = None
+record_duration = None
 
 # create aubio pitch detection (first argument is method, "default" is
 # "yinfft", can also be "yin", "mcomb", fcomb", "schmitt").
@@ -53,12 +46,9 @@ pitcher.set_unit("Hz")
 # ignore frames under this level (dB)
 pitcher.set_silence(-40)
 
-
-print("*** starting recording")
 while True:
     try:
         audiobuffer = stream.read(buffer_size)
-        # signal = np.fromstring(audiobuffer, dtype=np.float32)
         # convert data to aubio float samples
         samples = np.frombuffer(audiobuffer, dtype=aubio.float_type)
         # pitch of current frame
@@ -66,25 +56,11 @@ while True:
         # compute energy of current block
         energy = np.sum(samples**2)/len(samples)
         # do something with the results
-        print("{:10.4f} {:10.4f}".format(freq,1000*energy))
+        print("{:10.4f} {:10.4f}".format(freq,1000*energy), flush=True)
 
-        # pitch = pitch_o(signal)[0]
-        # confidence = pitch_o.get_confidence()
-
-        # print("{} / {}".format(pitch,confidence))
-
-        # if outputsink:
-        #     outputsink(signal, len(signal))
-
-        # if record_duration:
-        #     total_frames += len(signal)
-        #     if record_duration * samplerate < total_frames:
-        #         break
     except KeyboardInterrupt:
-        print("*** Ctrl+C pressed, exiting")
         break
 
-print("*** done recording")
 stream.stop_stream()
 stream.close()
 p.terminate()
